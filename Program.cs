3d6cef167b862e993fc94520df51b68417e179e9
prathameshbhatkar2@gmail.com <prathameshbhatkar2@gmail.com>
2020-11-16 16:29:51 +0530

    ok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
